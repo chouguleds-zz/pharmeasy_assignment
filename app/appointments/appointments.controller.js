32b1a54749439652c3e3a57b7cc02496d78ba24b
Deepak Chougule <chougule.ds@gmail.com>
2017-11-20 16:43:51 +0530

    test cases added and code refactored

--- app/appointments/appointments.controller.js
+++ app/appointments/appointments.controller.js
@@ -4,6 +4,33 @@ const Appointment = require('./appointments.model')
 const User = require('../users/users.model')
 
 /**
+ * Check if appointment is already booked or not
+ *
+ * @param patient email of patient
+ * @param doctor email of doctor
+ * @param time appointment time
+ */
+const _isAppointAlreadyBooked = async function (patient, doctor, time) {
+
+  const query = {
+    consulting_doctor: doctor,
+    patient: patient,
+    from_time: {
+      $lte: time
+    },
+    to_time: {
+      $gt: time
+    }
+  }
+  const appointment = await Appointment.find(query)
+  if (appointment.length !== 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+/**
  * Create appointment by the patient
  *
  * @param req
@@ -22,14 +49,21 @@ exports.create = async function (req, res) {
         message: 'Doctor not found.'
       })
     }
+    const isAppointAlreadyBooked = await _isAppointAlreadyBooked(req.user.email, req.body.consulting_doctor, req.body.from_time)
 
+    if (isAppointAlreadyBooked === true) {
+
+      return res.status(200).json({
+        success: false,
+        message: 'Already booked.'
+      })
+    }
     const appointment = new Appointment({
       patient: req.user.email,
       consulting_doctor: req.body.consulting_doctor,
       from_time: req.body.from_time,
       to_time: req.body.to_time
     })
-
     await appointment.save()
 
     return res.status(200).json({
